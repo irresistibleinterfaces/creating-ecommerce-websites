121ddf3aff063cfc843e7e613f6909b1c8931590
Kylie Copeland <irresistibleinterfaces@gmail.com>
2025-12-03 07:02:05 +1100

    Featured Categories
    Updated HTML, CSS, and JS files to add the next section, "Featured Categories". These are essentially the names of the groups the products will be categorised by, along with an accompanying image.

--- js/script.js
+++ js/script.js
@@ -33,5 +33,17 @@
             loop:true
         });
     });
+
+
+     /*--- FOR PRODUCT SLIDER ---------------------------*/
+    $(document).ready(function() {
+        $('#autoWidth').lightSlider({
+            autoWidth:true,
+            loop:true,
+            onSliderLoad: function() {
+                $('#autoWidth').removeClass('cS-hidden');
+            } 
+        });  
+    });
   
   
